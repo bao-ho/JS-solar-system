36b4b9e05fe95cccb17ffd5d91a9fc69b877d55d
Bao Ho <bao.hodang@gmail.com>
2021-12-03 12:18:41 +0100

    fix: version 1

---
+++ off-axis-sphere.js
@@ -0,0 +1,90 @@
+(function () {
+  let _onload = function () {
+    let canvastag = document.getElementById("canvas-donut");
+    let tmr2 = undefined;
+    let ctx = canvastag.getContext("2d");
+    const { width, height } = ctx.canvas;
+    ctx.fillStyle = "#000";
+    ctx.fillRect(0, 0, width, height);
+    let sun;
+    const createSphere = ({ R1, R2, K1, K2, A0, B, X }) => {
+      let A = 0;
+      const render = function () {
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, width, height);
+        // precompute cosines and sines of A, B, theta, phi, same as before
+        let cA = Math.cos(A),
+          sA = Math.sin(A),
+          cB = Math.cos(B),
+          sB = Math.sin(B);
+        for (let j = -1.57; j <= 1.57; j += 0.2) {
+          // j <=> theta
+          let ct = Math.cos(j),
+            st = Math.sin(j); // cosine theta, sine theta
+          for (i = 0; i < 6.28; i += 0.2) {
+            // i <=> phi
+            let sp = Math.sin(i),
+              cp = Math.cos(i); // cosine phi, sine phi
+            let ox = R1 * ct, // object x, y = (R2,0,0) + (R1 cos theta, R1 sin theta, 0)
+              oy = R1 * st;
+            // let x = ox * cp + X; // final 3D x coordinate
+            // let y = oy; // final 3D y
+            // let ooz = 1 / (K2 + ox * sp); // one over z
+            let x = ox * cp + X; // final 3D x coordinate
+            let y = -ox * sA * sp + oy * cA; // final 3D y
+            let ooz = 1 / (K2 + cA * ox * sp + sA * oy); // one over z
+            let xp = width / 2 + K1 * ooz * x; // x' = screen space coordinate, translated and scaled to fit our 320x240 canvas element
+            let yp = height / 2 - K1 * ooz * y; // y' (it's negative here because in our output, positive y goes down but in our 3D space, positive y goes up)
+            // luminance, scaled back to 0 to 1
+            const L = 0.7 * (cA * ct * sp - sA * st + cA * st - ct * sA * sp);
+            if (L > 0) {
+              ctx.fillStyle = "rgba(255,255,255," + L + ")";
+              ctx.fillRect(xp, yp, 2, 2);
+            }
+          }
+        }
+      };
+      const api = {
+        render,
+        rotateAroundXAxis(deltaA) {
+          A += deltaA;
+          this.render();
+        },
+      };
+      return api;
+    };
+
+    // HAMMER JS
+    var myElement = document.getElementById("canvas-donut");
+    var mc = new Hammer(myElement);
+    mc.get("pan").set({ direction: Hammer.DIRECTION_ALL });
+    mc.on("panleft panright panup pandown tap press", function (ev) {
+      switch (ev.type) {
+        case "panup":
+          earth.rotateAroundXAxis(0.2);
+          break;
+        case "pandown":
+          earth.rotateAroundXAxis(-0.2);
+          break;
+        default:
+          break;
+      }
+    });
+
+    window.anim2 = function () {
+      if (tmr2 === undefined) {
+        tmr2 = setInterval(sphere, 50, 0.07, 0.03);
+      } else {
+        clearInterval(tmr2);
+        tmr2 = undefined;
+      }
+    };
+    // sun = createSphere({ R1: 100, R2: 0, K1: 150, K2: 300, B: 0, X: 0 });
+    // sun.render();
+    earth = createSphere({ R1: 50, R2: 0, K1: 2500, K2: 3000, B: 0, X: 0 });
+    earth.render();
+  };
+
+  if (document.all) window.attachEvent("onload", _onload);
+  else window.addEventListener("load", _onload, false);
+})();
--- self-rotating-sphere.js
+++ self-rotating-sphere.js
@@ -8,35 +8,30 @@
     ctx.fillRect(0, 0, width, height);
     let sun;
     const createSphere = ({ R1, R2, K1, K2, A0, B, X }) => {
-      let A = 0;
+      let C = 0;
       const render = function () {
         ctx.fillStyle = "#000";
         ctx.fillRect(0, 0, width, height);
-        // precompute cosines and sines of A, B, theta, phi, same as before
-        let cA = Math.cos(A),
-          sA = Math.sin(A),
-          cB = Math.cos(B),
-          sB = Math.sin(B);
         for (let j = -1.57; j <= 1.57; j += 0.2) {
           // j <=> theta
           let ct = Math.cos(j),
             st = Math.sin(j); // cosine theta, sine theta
           for (i = 0; i < 6.28; i += 0.2) {
             // i <=> phi
-            let sp = Math.sin(i),
-              cp = Math.cos(i); // cosine phi, sine phi
+            let sp = Math.sin(i + C),
+              cp = Math.cos(i + C); // cosine phi, sine phi
             let ox = R1 * ct, // object x, y = (R2,0,0) + (R1 cos theta, R1 sin theta, 0)
               oy = R1 * st;
             // let x = ox * cp + X; // final 3D x coordinate
             // let y = oy; // final 3D y
             // let ooz = 1 / (K2 + ox * sp); // one over z
-            let x = ox * cp + X; // final 3D x coordinate
-            let y = -ox * sA * sp + oy * cA; // final 3D y
-            let ooz = 1 / (K2 + cA * ox * sp + sA * oy); // one over z
+            let x = ox * cp; // final 3D x coordinate
+            let y = oy; // final 3D y
+            let ooz = 1 / (K2 + ox * sp); // one over z
             let xp = width / 2 + K1 * ooz * x; // x' = screen space coordinate, translated and scaled to fit our 320x240 canvas element
             let yp = height / 2 - K1 * ooz * y; // y' (it's negative here because in our output, positive y goes down but in our 3D space, positive y goes up)
             // luminance, scaled back to 0 to 1
-            const L = 0.7 * (cA * ct * sp - sA * st + cA * st - ct * sA * sp);
+            const L = 0.7 * (st - ct * sp);
             if (L > 0) {
               ctx.fillStyle = "rgba(255,255,255," + L + ")";
               ctx.fillRect(xp, yp, 2, 2);
@@ -49,6 +44,10 @@
         rotateAroundXAxis(deltaA) {
           A += deltaA;
           this.render();
+        },
+        rotateAroundYAxis(deltaC) {
+          C += deltaC;
+          render();
         },
       };
       return api;
@@ -66,6 +65,12 @@
         case "pandown":
           earth.rotateAroundXAxis(-0.2);
           break;
+        case "panleft":
+          earth.rotateAroundYAxis(0.017);
+          break;
+        case "panright":
+          earth.rotateAroundYAxis(-0.017);
+          break;
         default:
           break;
       }
@@ -73,7 +78,7 @@
 
     window.anim2 = function () {
       if (tmr2 === undefined) {
-        tmr2 = setInterval(sphere, 50, 0.07, 0.03);
+        tmr2 = setInterval(earth.rotateAroundYAxis, 50, 0.03);
       } else {
         clearInterval(tmr2);
         tmr2 = undefined;
@@ -81,7 +86,7 @@
     };
     // sun = createSphere({ R1: 100, R2: 0, K1: 150, K2: 300, B: 0, X: 0 });
     // sun.render();
-    earth = createSphere({ R1: 50, R2: 0, K1: 2500, K2: 3000, B: 0, X: 300 });
+    earth = createSphere({ R1: 200, R2: 0, K1: 2500, K2: 3000, B: 0, X: 0 });
     earth.render();
   };
 
